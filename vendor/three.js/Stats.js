51a4016e4f0f2f9c533a9012dc3a37ce68f77afe
Davide Della Casa <davidedc@gmail.com>
2012-04-18 22:07:15 +0100

    1) added the requestAnimationFrame mode with limited FPS 2) put the stats badge to fit nicely on the top right 3) restyled a little bit the menu

--- vendor/three.js/Stats.js
+++ vendor/three.js/Stats.js
@@ -1,8 +1,196 @@
-// stats.js r8 - http://github.com/mrdoob/stats.js
-var Stats=function(){var h,a,n=0,o=0,i=Date.now(),u=i,p=i,l=0,q=1E3,r=0,e,j,f,b=[[16,16,48],[0,255,255]],m=0,s=1E3,t=0,d,k,g,c=[[16,48,16],[0,255,0]];h=document.createElement("div");h.style.cursor="pointer";h.style.width="80px";h.style.opacity="0.9";h.style.zIndex="10001";h.addEventListener("mousedown",function(a){a.preventDefault();n=(n+1)%2;n==0?(e.style.display="block",d.style.display="none"):(e.style.display="none",d.style.display="block")},!1);e=document.createElement("div");e.style.textAlign=
-"left";e.style.lineHeight="1.2em";e.style.backgroundColor="rgb("+Math.floor(b[0][0]/2)+","+Math.floor(b[0][1]/2)+","+Math.floor(b[0][2]/2)+")";e.style.padding="0 0 3px 3px";h.appendChild(e);j=document.createElement("div");j.style.fontFamily="Helvetica, Arial, sans-serif";j.style.fontSize="9px";j.style.color="rgb("+b[1][0]+","+b[1][1]+","+b[1][2]+")";j.style.fontWeight="bold";j.innerHTML="FPS";e.appendChild(j);f=document.createElement("div");f.style.position="relative";f.style.width="74px";f.style.height=
-"30px";f.style.backgroundColor="rgb("+b[1][0]+","+b[1][1]+","+b[1][2]+")";for(e.appendChild(f);f.children.length<74;)a=document.createElement("span"),a.style.width="1px",a.style.height="30px",a.style.cssFloat="left",a.style.backgroundColor="rgb("+b[0][0]+","+b[0][1]+","+b[0][2]+")",f.appendChild(a);d=document.createElement("div");d.style.textAlign="left";d.style.lineHeight="1.2em";d.style.backgroundColor="rgb("+Math.floor(c[0][0]/2)+","+Math.floor(c[0][1]/2)+","+Math.floor(c[0][2]/2)+")";d.style.padding=
-"0 0 3px 3px";d.style.display="none";h.appendChild(d);k=document.createElement("div");k.style.fontFamily="Helvetica, Arial, sans-serif";k.style.fontSize="9px";k.style.color="rgb("+c[1][0]+","+c[1][1]+","+c[1][2]+")";k.style.fontWeight="bold";k.innerHTML="MS";d.appendChild(k);g=document.createElement("div");g.style.position="relative";g.style.width="74px";g.style.height="30px";g.style.backgroundColor="rgb("+c[1][0]+","+c[1][1]+","+c[1][2]+")";for(d.appendChild(g);g.children.length<74;)a=document.createElement("span"),
-a.style.width="1px",a.style.height=Math.random()*30+"px",a.style.cssFloat="left",a.style.backgroundColor="rgb("+c[0][0]+","+c[0][1]+","+c[0][2]+")",g.appendChild(a);return{domElement:h,update:function(){i=Date.now();m=i-u;s=Math.min(s,m);t=Math.max(t,m);k.textContent=m+" MS ("+s+"-"+t+")";var a=Math.min(30,30-m/200*30);g.appendChild(g.firstChild).style.height=a+"px";u=i;o++;if(i>p+1E3)l=Math.round(o*1E3/(i-p)),q=Math.min(q,l),r=Math.max(r,l),j.textContent=l+" FPS ("+q+"-"+r+")",a=Math.min(30,30-l/
-100*30),f.appendChild(f.firstChild).style.height=a+"px",p=i,o=0}}};
+/**
+ * @author mr.doob / http://mrdoob.com/
+ */
 
+var badgeHeight = 16;
+
+var Stats = function () {
+
+	var _container, _bar, _mode = 0, _modes = 2,
+	_frames = 0, _time = Date.now(), _timeLastFrame = _time, _timeLastSecond = _time,
+	_fps = 0, _fpsMin = 1000, _fpsMax = 0, _fpsDiv, _fpsText, _fpsGraph,
+	_fpsColors = [ [ 50, 50, 50 ], [ 255, 255, 255 ] ],
+	_ms = 0, _msMin = 1000, _msMax = 0, _msDiv, _msText, _msGraph,
+	_msColors = [ [ 50, 50, 50 ], [ 255, 255, 255 ] ];
+
+	_container = document.createElement( 'div' );
+	_container.style.cursor = 'pointer';
+	_container.style.width = '74px';
+	//_container.style.opacity = '0.9';
+	_container.style.zIndex = '10001';
+	_container.addEventListener( 'mousedown', function ( event ) {
+
+		event.preventDefault();
+
+		_mode = ( _mode + 1 ) % _modes;
+
+		if ( _mode == 0 ) {
+
+			_fpsDiv.style.display = 'block';
+			_msDiv.style.display = 'none';
+
+		} else {
+
+			_fpsDiv.style.display = 'none';
+			_msDiv.style.display = 'block';
+
+		}
+
+	}, false );
+
+	// fps
+
+	_fpsDiv = document.createElement( 'div' );
+	_fpsDiv.style.textAlign = 'right';
+	_fpsDiv.style.lineHeight = '1.2em';
+	_fpsDiv.style.backgroundColor = 'rgb(50,50,50)';
+	_fpsDiv.style.padding = '0 0 0px 0px';
+	_container.appendChild( _fpsDiv );
+
+	_fpsText = document.createElement( 'div' );
+	_fpsText.style.fontFamily = 'Helvetica, Arial, sans-serif';
+	_fpsText.style.fontSize = '9px';
+	_fpsText.style.color = 'rgb(' + _fpsColors[ 1 ][ 0 ] + ',' + _fpsColors[ 1 ][ 1 ] + ',' + _fpsColors[ 1 ][ 2 ] + ')';
+	_fpsText.style.fontWeight = 'bold';
+	_fpsText.innerHTML = 'FPS';
+	_fpsDiv.appendChild( _fpsText );
+
+	_fpsGraph = document.createElement( 'div' );
+	_fpsGraph.style.position = 'relative';
+	_fpsGraph.style.width = '74px';
+	_fpsGraph.style.height = badgeHeight + 'px';
+	_fpsGraph.style.backgroundColor = 'rgb(' + _fpsColors[ 1 ][ 0 ] + ',' + _fpsColors[ 1 ][ 1 ] + ',' + _fpsColors[ 1 ][ 2 ] + ')';
+	_fpsDiv.appendChild( _fpsGraph );
+
+	while ( _fpsGraph.children.length < 74 ) {
+
+		_bar = document.createElement( 'span' );
+		_bar.style.width = '1px';
+		_bar.style.height = badgeHeight + 'px';
+		_bar.style.cssFloat = 'left';
+		_bar.style.backgroundColor = 'rgb(' + _fpsColors[ 0 ][ 0 ] + ',' + _fpsColors[ 0 ][ 1 ] + ',' + _fpsColors[ 0 ][ 2 ] + ')';
+		_fpsGraph.appendChild( _bar );
+
+	}
+
+	// ms
+
+	_msDiv = document.createElement( 'div' );
+	_msDiv.style.textAlign = 'right';
+	_msDiv.style.lineHeight = '1.2em';
+	_msDiv.style.backgroundColor = 'rgb(50,50,50)';
+	_msDiv.style.padding = '0 0 0px 0px';
+	_msDiv.style.display = 'none';
+	_container.appendChild( _msDiv );
+
+	_msText = document.createElement( 'div' );
+	_msText.style.fontFamily = 'Helvetica, Arial, sans-serif';
+	_msText.style.fontSize = '9px';
+	_msText.style.color = 'rgb(' + _msColors[ 1 ][ 0 ] + ',' + _msColors[ 1 ][ 1 ] + ',' + _msColors[ 1 ][ 2 ] + ')';
+	_msText.style.fontWeight = 'bold';
+	_msText.innerHTML = 'MS';
+	_msDiv.appendChild( _msText );
+
+	_msGraph = document.createElement( 'div' );
+	_msGraph.style.position = 'relative';
+	_msGraph.style.width = '74px';
+	_msGraph.style.height = badgeHeight + 'px';
+	_msGraph.style.backgroundColor = 'rgb(' + _msColors[ 1 ][ 0 ] + ',' + _msColors[ 1 ][ 1 ] + ',' + _msColors[ 1 ][ 2 ] + ')';
+	_msDiv.appendChild( _msGraph );
+
+	while ( _msGraph.children.length < 74 ) {
+
+		_bar = document.createElement( 'span' );
+		_bar.style.width = '1px';
+		_bar.style.height = Math.random() * badgeHeight + 'px';
+		_bar.style.cssFloat = 'left';
+		_bar.style.backgroundColor = 'rgb(' + _msColors[ 0 ][ 0 ] + ',' + _msColors[ 0 ][ 1 ] + ',' + _msColors[ 0 ][ 2 ] + ')';
+		_msGraph.appendChild( _bar );
+
+	}
+
+	var _updateGraph = function ( dom, value ) {
+
+		var child = dom.appendChild( dom.firstChild );
+		child.style.height = value + 'px';
+
+	}
+
+	return {
+
+		getDomElement: function () {
+
+			return _container;
+
+		},
+
+		getFps: function () {
+
+			return _fps;
+
+		},
+
+		getFpsMin: function () {
+
+			return _fpsMin;
+
+		},
+
+		getFpsMax: function () {
+
+			return _fpsMax;
+
+		},
+
+		getMs: function () {
+
+			return _ms;
+
+		},
+
+		getMsMin: function () {
+
+			return _msMin;
+
+		},
+
+		getMsMax: function () {
+
+			return _msMax;
+
+		},
+
+		update: function () {
+
+			_time = Date.now();
+
+			_ms = _time - _timeLastFrame;
+			_msMin = Math.min( _msMin, _ms );
+			_msMax = Math.max( _msMax, _ms );
+
+			_msText.textContent = _ms + ' MS (' + _msMin + '-' + _msMax + ')';
+			_updateGraph( _msGraph, Math.min( badgeHeight, badgeHeight - ( _ms / 200 ) * badgeHeight ) );
+
+			_timeLastFrame = _time;
+
+			_frames ++;
+
+			if ( _time > _timeLastSecond + 1000 ) {
+
+				_fps = Math.round( ( _frames * 1000 ) / ( _time - _timeLastSecond ) );
+				_fpsMin = Math.min( _fpsMin, _fps );
+				_fpsMax = Math.max( _fpsMax, _fps );
+
+				_fpsText.textContent = _fps + ' FPS (' + _fpsMin + '-' + _fpsMax + ')';
+				_updateGraph( _fpsGraph, Math.min( badgeHeight, badgeHeight - ( _fps / 100 ) * badgeHeight ) );
+
+				_timeLastSecond = _time;
+				_frames = 0;
+
+			}
+
+		}
+
+	};
+
+};
