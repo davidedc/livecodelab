b4255e9d887f9afada7b754e7049bf67300d424c
Davide Della Casa <davidedc@gmail.com>
2012-07-15 19:49:29 +0100

    a) separated matrix-related-commands (scale, translate, etc.) into separate file b) moved background() function a bit

--- livecodelab.js
+++ livecodelab.js
@@ -745,17 +745,6 @@ window.onerror = function(msg, url, linenumber) {
   return true;
 }
 
-function background(r, g, b) {
-
-  clearDisplayList();
-
-  // canvas renderer
-  theColor = new THREE.Color();
-  theColor.setRGB(r, g, b);
-  //renderer.setClearColor(theColor,1);
-
-
-}
 
 // animation loop
 var loopInterval;
@@ -939,105 +928,19 @@ var shrinkFakeText = function(e) {
   }
 
 
-var parentObject, rootObject, rotate, move;
-parentObject = 0;
-rootObject = 0;
-var currentObject;
 
 
-var matrixStack = [];
-pushMatrix = function() {
-  matrixStack.push(worldMatrix);
-  worldMatrix = (new THREE.Matrix4()).copy(worldMatrix);
+
+function background(r, g, b) {
+
+  clearDisplayList();
+
+  // canvas renderer
+  theColor = new THREE.Color();
+  theColor.setRGB(r, g, b);
+  //renderer.setClearColor(theColor,1);
+
 }
-
-popMatrix = function() {
-  if (matrixStack.length !== 0) worldMatrix = matrixStack.pop();
-  else worldMatrix.identity();
-}
-
-resetMatrix = function() {
-  worldMatrix.identity();
-}
-
-move = function(a, b, c) {
-  if (arguments.length === 0) {
-    a = Math.sin(time / 500);
-    b = Math.cos(time / 500);
-    c = a;
-  } else if (arguments.length == 1) {
-    b = a;
-    c = a;
-  } else if (arguments.length == 2) {
-    c = 0;
-  }
-
-  /*
-  currentObject = new THREE.Object3D();
-  currentObject.position.x = a;
-  currentObject.position.y = b;
-  currentObject.position.z = c;
-  parentObject.add(currentObject);
-  parentObject = currentObject;
-  */
-  worldMatrix.translate(new THREE.Vector3(a, b, c));
-};
-
-rotate = function(a, b, c) {
-
-  if (arguments.length === 0) {
-    a = time / 1000;
-    b = a;
-    c = a;
-  } else if (arguments.length == 1) {
-    b = a;
-    c = a;
-  } else if (arguments.length == 2) {
-    c = 0;
-  }
-
-  /*
-  currentObject = new THREE.Object3D();
-  currentObject.rotation.x = a;
-  currentObject.rotation.y = b;
-  currentObject.rotation.z = c;
-  parentObject.add(currentObject);
-  parentObject = currentObject;
-  */
-  //worldMatrix.setRotationFromEuler(new THREE.Vector3(a,b,c));
-  worldMatrix.rotateX(a).rotateY(b).rotateZ(c);
-
-};
-
-scale = function(a, b, c) {
-  if (arguments.length === 0) {
-    a = 1 + Math.sin(time / 500) / 4;
-    b = a;
-    c = a;
-  } else if (arguments.length == 1) {
-    b = a;
-    c = a;
-  } else if (arguments.length == 2) {
-    c = 1;
-  }
-
-  // odd things happen setting scale to zero
-  if (a > -0.000000001 && a < 0.000000001) a = 0.000000001;
-  if (b > -0.000000001 && b < 0.000000001) b = 0.000000001;
-  if (c > -0.000000001 && c < 0.000000001) c = 0.000000001;
-
-  /*
-  currentObject = new THREE.Object3D();
-  currentObject.scale.x = a;
-  currentObject.scale.y = b;
-  currentObject.scale.z = c;
-  parentObject.add(currentObject);
-  parentObject = currentObject;
-  */
-  worldMatrix.scale(new THREE.Vector3(a, b, c));
-
-};
-
 
 // TODO Note that lines have a "solid fill" mode
 // and something similar to the normalMaterial mode
