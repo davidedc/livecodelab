56f1219515863d3881b382ce898c7b975d37f57a
Davide Della Casa <davidedc@gmail.com>
2012-07-16 01:37:50 +0100

    log function is now self-standing rather than being attached to the window object…
    this is because the Closure compiler would otherwise complain. Also
    replaced a "this" with "window" (since removing "window" makes "this"
    miss its reference)

--- helper-functions.js
+++ helper-functions.js
@@ -7,10 +7,10 @@
 
 // usage: log('inside coolFunc',this,arguments);
 // http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function(){
+var log = function(){
   log.history = log.history || [];   // store logs to an array for reference
   log.history.push(arguments);
-  if(this.console){
+  if(window.console){
     console.log( Array.prototype.slice.call(arguments) );
   }
 };
