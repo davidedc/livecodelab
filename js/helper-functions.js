fc7ccde59fad04d650d161180a284a1ca39dc28b
Guy John <guy@rumblesan.com>
2012-11-17 18:26:39 +0000

    Removing log function from helper functions
    Tried to replace this, then realised that it was getting over-ridden by
    the Math.log function anyway so just removing it.

--- js/helper-functions.js
+++ js/helper-functions.js
@@ -1,25 +1,6 @@
 /*jslint browser: true, devel: true */
 /*global editor, $ */
 
-// log() – A lightweight wrapper for console.log
-// There are a few things that a console.log wrapper can and should do:
-//  - Prevent errors if a console isn't around (i.e. IE)
-//  - Maintain a history of logs, so you can look in the past if your console is added afterwards (e.g. firebug lite)
-//  - Normalize the browser differences in console integration (e.g. when passing multiple arguments into console.log())
-//  - For something you type regularly, make it quicker to type for the lazy among us.
-
-// usage: log('inside coolFunc',this,arguments);
-// http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-var log = function () {
-    /*
-  log.history = log.history || [];   // store logs to an array for reference
-  log.history.push(arguments);
-  if(window.console){
-    console.log( Array.prototype.slice.call(arguments) );
-  }
-  */
-};
-
 ///////////// START OF MOUSEWHEEL HANDLER CODE
 // I couldn't quite get the exact Javascript plugin below to
 // work in its original form, so I only took the
