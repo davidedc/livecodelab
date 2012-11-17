3a86735dfd8018152e257180dd31a13eec8222f8
Guy John <guy@rumblesan.com>
2012-11-17 20:21:10 +0000

    Moving editor creation into editor.js file

--- js/editor.js
+++ js/editor.js
@@ -1,2 +1,28 @@
-// Editor, Mouse wheel handling, fullscreen and resizing management
+/*jslint browser: true, devel: true */
+/*global CodeMirror, registerCode, suspendDimmingAndCheckIfLink */
 
+var createEditor = function () {
+    var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+        lineNumbers: false,
+        indentWithTabs: true,
+        tabSize: 1,
+        indentUnit: 1,
+        lineWrapping: true,
+        // We want the code editor to always have focus
+        // since there is nothing else to type into.
+        // One of those little wonders: you have to pause a little
+        // before giving the editor focus, otherwise for some reason
+        // the focus is not regained. Go figure.
+        onBlur: (function () {
+            setTimeout('editor.focus()', 30);
+        }),
+        onChange: (function () {
+            registerCode();
+        }),
+        mode: "livecodelab",
+        onCursorActivity: (function () {
+            suspendDimmingAndCheckIfLink();
+        })
+    });
+    return editor;
+};
--- js/init.js
+++ js/init.js
@@ -146,29 +146,7 @@ var startEnvironment = function() {
   $('#startingCourtainScreen').fadeOut();
   $("#formCode").css('opacity', 0);
 
-editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-  lineNumbers: false,
-  indentWithTabs: true,
-  tabSize: 1,
-  indentUnit: 1,
-  lineWrapping: true,
-  // We want the code editor to always have focus
-  // since there is nothing else to type into.
-  // One of those little wonders: you have to pause a little
-  // before giving the editor focus, otherwise for some reason
-  // the focus is not regained. Go figure.
-  onBlur: (function() {
-    setTimeout('editor.focus()', 30);
-  }),
-  onChange: (function() {
-    registerCode();
-  }),
-  mode: "livecodelab",
-  onCursorActivity: (function() {
-    suspendDimmingAndCheckIfLink();
-  })
-  //onScroll: (function(){alert('scroll')})
-});
+    editor = createEditor();
 
 
 editor.setOption("theme", 'night');
