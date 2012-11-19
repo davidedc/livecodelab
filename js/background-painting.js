7240398cf834b9a5dd8833a6b2e0e8225e57750f
Guy John <guy@rumblesan.com>
2012-11-19 23:21:15 +0000

    Moving background painter functions into an object
    Though making sure that background and simple gradient are made global

--- js/background-painting.js
+++ js/background-painting.js
@@ -1,136 +1,138 @@
 /*jslint browser: true, devel: true */
 /*global $, color, logger, backgroundSceneContext, scaledBackgroundHeight, scaledBackgroundWidth */
 
-var gradStack = [];
-var defaultGradientColor1 = orange;
-var defaultGradientColor2 = red;
-var defaultGradientColor3 = black;
-var whichDefaultBackground;
-var currentGradientStackValue = '';
-var previousGradientStackValue = 0;
-
-
-// This needs to be global so it can be run by the draw function
-var simpleGradient = function (a, b, c, d) {
+var createBackgroundPainter = function () {
 
     'use strict';
 
-    currentGradientStackValue = currentGradientStackValue + " " + a + "" + b + "" + c + "" + d + "null ";
-    gradStack.push({
-        gradStacka: a,
-        gradStackb: b,
-        gradStackc: c,
-        gradStackd: d,
-        solid: null
-    });
+    var gradStack = [],
+        defaultGradientColor1 = orange,
+        defaultGradientColor2 = red,
+        defaultGradientColor3 = black,
+        whichDefaultBackground,
+        currentGradientStackValue = '',
+        previousGradientStackValue = 0,
+        BackgroundPainter = {};
 
-};
+    // This needs to be global so it can be run by the draw function
+    BackgroundPainter.simpleGradient = function (a, b, c, d) {
 
+        currentGradientStackValue = currentGradientStackValue + " " + a + "" + b + "" + c + "" + d + "null ";
+        gradStack.push({
+            gradStacka: a,
+            gradStackb: b,
+            gradStackc: c,
+            gradStackd: d,
+            solid: null
+        });
 
-var pickRandomDefaultGradient = function () {
+    };
 
-    'use strict';
+    // This needs to be global so it can be run by the draw function
+    BackgroundPainter.background = function () {
 
-    if (whichDefaultBackground === undefined) {
-        whichDefaultBackground = Math.floor(Math.random() * 5);
-    } else {
-        whichDefaultBackground = (whichDefaultBackground + 1) % 5;
-    }
+        // [todo] should the screen be cleared when you invoke
+        // the background command? (In processing it's not)
 
-    if (whichDefaultBackground === 0) {
-        defaultGradientColor1 = orange;
-        defaultGradientColor2 = red;
-        defaultGradientColor3 = black;
-        $("#fakeStartingBlinkingCursor").css('color', 'white');
-    } else if (whichDefaultBackground === 1) {
-        defaultGradientColor1 = white;
-        defaultGradientColor2 = khaki;
-        defaultGradientColor3 = peachpuff;
-        $("#fakeStartingBlinkingCursor").css('color', 'LightPink');
-    } else if (whichDefaultBackground === 2) {
-        defaultGradientColor1 = lightsteelblue;
-        defaultGradientColor2 = lightcyan;
-        defaultGradientColor3 = paleturquoise;
-        $("#fakeStartingBlinkingCursor").css('color', 'CadetBlue');
-    } else if (whichDefaultBackground === 3) {
-        defaultGradientColor1 = silver;
-        defaultGradientColor2 = lightgrey;
-        defaultGradientColor3 = gainsboro;
-        $("#fakeStartingBlinkingCursor").css('color', 'white');
-    } else if (whichDefaultBackground === 4) {
-        defaultGradientColor1 = color(155, 255, 155);
-        defaultGradientColor2 = color(155, 255, 155);
-        defaultGradientColor3 = color(155, 255, 155);
-        $("#fakeStartingBlinkingCursor").css('color', 'DarkOliveGreen');
-    }
-};
+        var a = color(arguments[0], arguments[1], arguments[2], arguments[3]);
+        //  logger("adding solid background to stack");
+        //if (a===undefined) a = color(0);
+        currentGradientStackValue = currentGradientStackValue + " null null null null " + a + " ";
+        gradStack.push({
+            gradStacka: undefined,
+            gradStackb: undefined,
+            gradStackc: undefined,
+            gradStackd: undefined,
+            solid: a
+        });
+    };
 
-var resetGradientStack = function () {
+    BackgroundPainter.pickRandomDefaultGradient = function () {
 
-    'use strict';
+        if (whichDefaultBackground === undefined) {
+            whichDefaultBackground = Math.floor(Math.random() * 5);
+        } else {
+            whichDefaultBackground = (whichDefaultBackground + 1) % 5;
+        }
 
-    currentGradientStackValue = "";
-    // we could be more efficient and
-    // reuse the previous stack elements
-    // but I don't think it matters here
-    gradStack = [];
+        if (whichDefaultBackground === 0) {
+            defaultGradientColor1 = orange;
+            defaultGradientColor2 = red;
+            defaultGradientColor3 = black;
+            $("#fakeStartingBlinkingCursor").css('color', 'white');
+        } else if (whichDefaultBackground === 1) {
+            defaultGradientColor1 = white;
+            defaultGradientColor2 = khaki;
+            defaultGradientColor3 = peachpuff;
+            $("#fakeStartingBlinkingCursor").css('color', 'LightPink');
+        } else if (whichDefaultBackground === 2) {
+            defaultGradientColor1 = lightsteelblue;
+            defaultGradientColor2 = lightcyan;
+            defaultGradientColor3 = paleturquoise;
+            $("#fakeStartingBlinkingCursor").css('color', 'CadetBlue');
+        } else if (whichDefaultBackground === 3) {
+            defaultGradientColor1 = silver;
+            defaultGradientColor2 = lightgrey;
+            defaultGradientColor3 = gainsboro;
+            $("#fakeStartingBlinkingCursor").css('color', 'white');
+        } else if (whichDefaultBackground === 4) {
+            defaultGradientColor1 = color(155, 255, 155);
+            defaultGradientColor2 = color(155, 255, 155);
+            defaultGradientColor3 = color(155, 255, 155);
+            $("#fakeStartingBlinkingCursor").css('color', 'DarkOliveGreen');
+        }
+    };
 
-    //simpleGradient(color(70),color(30),color(0),1);
-    simpleGradient(defaultGradientColor1, defaultGradientColor2, defaultGradientColor3);
+    BackgroundPainter.resetGradientStack = function () {
 
-};
+        currentGradientStackValue = "";
+        // we could be more efficient and
+        // reuse the previous stack elements
+        // but I don't think it matters here
+        gradStack = [];
 
-// This needs to be global so it can be run by the draw function
-var background = function () {
+        BackgroundPainter.simpleGradient(defaultGradientColor1, defaultGradientColor2, defaultGradientColor3);
+    };
 
-    'use strict';
+    BackgroundPainter.simpleGradientUpdateIfChanged = function () {
 
-    // [todo] should the screen be cleared when you invoke
-    // the background command? (In processing it's not)
+        var diagonal, radgrad, scanningGradStack;
+        if ((currentGradientStackValue !== previousGradientStackValue)) {
 
-    var a = color(arguments[0], arguments[1], arguments[2], arguments[3]);
-    //  logger("adding solid background to stack");
-    //if (a===undefined) a = color(0);
-    currentGradientStackValue = currentGradientStackValue + " null null null null " + a + " ";
-    gradStack.push({
-        gradStacka: undefined,
-        gradStackb: undefined,
-        gradStackc: undefined,
-        gradStackd: undefined,
-        solid: a
-    });
-};
+            previousGradientStackValue = currentGradientStackValue;
+            logger('repainting background');
+            diagonal = Math.sqrt(Math.pow(scaledBackgroundWidth / 2, 2) + Math.pow(scaledBackgroundHeight / 2, 2));
 
+            for (scanningGradStack = 0; scanningGradStack < gradStack.length; scanningGradStack++) {
 
-var simpleGradientUpdateIfChanged = function () {
+                if (gradStack[scanningGradStack].gradStacka !== undefined) {
+                    radgrad = backgroundSceneContext.createLinearGradient(scaledBackgroundWidth / 2, 0, scaledBackgroundWidth / 2, scaledBackgroundHeight);
+                    radgrad.addColorStop(0, color.toString(gradStack[scanningGradStack].gradStacka));
+                    radgrad.addColorStop(0.5, color.toString(gradStack[scanningGradStack].gradStackb));
+                    radgrad.addColorStop(1, color.toString(gradStack[scanningGradStack].gradStackc));
 
-    'use strict';
-
-    var diagonal, radgrad, scanningGradStack;
-    if ((currentGradientStackValue !== previousGradientStackValue)) {
-
-        previousGradientStackValue = currentGradientStackValue;
-        logger('repainting background');
-        diagonal = Math.sqrt(Math.pow(scaledBackgroundWidth / 2, 2) + Math.pow(scaledBackgroundHeight / 2, 2));
-
-        for (scanningGradStack = 0; scanningGradStack < gradStack.length; scanningGradStack++) {
-
-            if (gradStack[scanningGradStack].gradStacka !== undefined) {
-                radgrad = backgroundSceneContext.createLinearGradient(scaledBackgroundWidth / 2, 0, scaledBackgroundWidth / 2, scaledBackgroundHeight);
-                radgrad.addColorStop(0, color.toString(gradStack[scanningGradStack].gradStacka));
-                radgrad.addColorStop(0.5, color.toString(gradStack[scanningGradStack].gradStackb));
-                radgrad.addColorStop(1, color.toString(gradStack[scanningGradStack].gradStackc));
-
-                backgroundSceneContext.globalAlpha = 1.0;
-                backgroundSceneContext.fillStyle = radgrad;
-                backgroundSceneContext.fillRect(0, 0, scaledBackgroundWidth, scaledBackgroundHeight);
-            } else {
-                logger("solid background: " + gradStack[scanningGradStack].solid);
-                backgroundSceneContext.globalAlpha = 1.0;
-                backgroundSceneContext.fillStyle = color.toString(gradStack[scanningGradStack].solid);
-                backgroundSceneContext.fillRect(0, 0, scaledBackgroundWidth, scaledBackgroundHeight);
+                    backgroundSceneContext.globalAlpha = 1.0;
+                    backgroundSceneContext.fillStyle = radgrad;
+                    backgroundSceneContext.fillRect(0, 0, scaledBackgroundWidth, scaledBackgroundHeight);
+                } else {
+                    logger("solid background: " + gradStack[scanningGradStack].solid);
+                    backgroundSceneContext.globalAlpha = 1.0;
+                    backgroundSceneContext.fillStyle = color.toString(gradStack[scanningGradStack].solid);
+                    backgroundSceneContext.fillRect(0, 0, scaledBackgroundWidth, scaledBackgroundHeight);
+                }
             }
         }
-    }
+
+    };
+
+    return BackgroundPainter;
 
 };
+
+var BackgroundPainter = createBackgroundPainter();
+
+// This needs to be global so it can be run by the draw function
+var simpleGradient = BackgroundPainter.simpleGradient;
+
+// This needs to be global so it can be run by the draw function
+var background = BackgroundPainter.background;
