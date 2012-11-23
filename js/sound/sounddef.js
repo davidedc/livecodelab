19e0f8c7b93db34d1824c84194d47cad5f1ff8a9
Davide Della Casa <davidedc@gmail.com>
2012-11-23 00:30:15 +0000

    revamp of the sound samples

--- js/sound/sounddef.js
+++ js/sound/sounddef.js
@@ -37,13 +37,9 @@ var createSoundDef = function (fileType) {
 
     soundDef.load('bing', './sound/audioFiles/start_bing');
 
-    soundDef.load('toc', './sound/audioFiles/AMB_BD_1');
-
     soundDef.load('highHatClosed', './sound/audioFiles/AMB_HHCL');
 
     soundDef.load('highHatOpen', './sound/audioFiles/AMB_HHOP');
-
-    soundDef.load('toc2', './sound/audioFiles/AMB_HTM');
 
     soundDef.load('toc3', './sound/audioFiles/AMB_LTM2');
 
@@ -52,8 +48,6 @@ var createSoundDef = function (fileType) {
     soundDef.load('snare', './sound/audioFiles/AMB_SN13');
 
     soundDef.load('snare2', './sound/audioFiles/AMB_SN_5');
-
-    soundDef.load('china', './sound/audioFiles/CHINA_1');
 
     soundDef.load('crash', './sound/audioFiles/CRASH_1');
 
@@ -65,12 +59,6 @@ var createSoundDef = function (fileType) {
 
     soundDef.load('glass', './sound/audioFiles/glass2');
 
-    soundDef.load('glass1', './sound/audioFiles/glass3');
-
-    soundDef.load('glass2', './sound/audioFiles/glass4');
-
-    soundDef.load('glass3', './sound/audioFiles/glass5');
-
     soundDef.load('thump', './sound/audioFiles/8938__patchen__piano-hits-hand-03v2');
 
     soundDef.load('lowFlash', './sound/audioFiles/9569__thanvannispen__industrial-low-flash04');
@@ -80,8 +68,6 @@ var createSoundDef = function (fileType) {
     soundDef.load('tranceKick2', './sound/audioFiles/24004__laya__dance-kick3');
 
     soundDef.load('tranceKick', './sound/audioFiles/33325__laya__trance-kick01');
-
-    soundDef.load('wosh', './sound/audioFiles/33960__krlox__pudricion-4');
 
     soundDef.load('voltage', './sound/audioFiles/49255__keinzweiter__bonobob-funk');
 
@@ -93,11 +79,47 @@ var createSoundDef = function (fileType) {
 
     soundDef.load('beepD', './sound/audioFiles/100708__steveygos93__bleep_d');
 
-    soundDef.load('beep', './sound/audioFiles/116508_Beep');
-
-    soundDef.load('hello', './sound/audioFiles/116508_Hello');
-
     soundDef.load('alienBeep', './sound/audioFiles/132389__blackie666__alienbleep');
+
+    soundDef.load('penta1', './sound/audioFiles/toneMatrix-1');
+
+    soundDef.load('penta2', './sound/audioFiles/toneMatrix-2');
+
+    soundDef.load('penta3', './sound/audioFiles/toneMatrix-3');
+
+    soundDef.load('penta4', './sound/audioFiles/toneMatrix-4');
+
+    soundDef.load('penta5', './sound/audioFiles/toneMatrix-5');
+
+    soundDef.load('penta6', './sound/audioFiles/toneMatrix-6');
+
+    soundDef.load('penta7', './sound/audioFiles/toneMatrix-7');
+
+    soundDef.load('penta8', './sound/audioFiles/toneMatrix-8');
+
+    soundDef.load('penta9', './sound/audioFiles/toneMatrix-9');
+
+    soundDef.load('penta10', './sound/audioFiles/toneMatrix-10');
+
+    soundDef.load('penta11', './sound/audioFiles/toneMatrix-11');
+
+    soundDef.load('penta12', './sound/audioFiles/toneMatrix-12');
+
+    soundDef.load('penta13', './sound/audioFiles/toneMatrix-13');
+
+    soundDef.load('penta14', './sound/audioFiles/toneMatrix-14');
+
+    soundDef.load('penta15', './sound/audioFiles/toneMatrix-15');
+
+    soundDef.load('penta16', './sound/audioFiles/toneMatrix-16');
+
+    soundDef.load('ciack', './sound/audioFiles/ciack');
+
+    soundDef.load('snap', './sound/audioFiles/snap');
+
+    soundDef.load('thump2', './sound/audioFiles/thump2');
+
+    soundDef.load('dish', './sound/audioFiles/dish');
 
     return soundDef;
 
