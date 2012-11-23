912ff020c4d08a457b45a9b58d49fd6e63e9fde9
Guy John <guy@rumblesan.com>
2012-11-23 15:03:59 +0000

    Creating BigCursor object creator
    All references to the bigcursor code now live in this object

--- js/big-cursor-animation.js
+++ js/big-cursor-animation.js
@@ -1,44 +1,68 @@
-var lastkey = 0;
+/*jslint browser: true, devel: true */
+/*global $, editor */
+
+
 var fakeText = true;
 
-document.onkeypress = function(e) {
-  if (fakeText && editor.getValue() !== "") shrinkFakeText(e);
-}
+var createBigCursor = function () {
 
-var shrinkFakeText = function(e) {
+    'use strict';
 
-    if (e !== undefined) {
-      var theEvent = e || window.event;
-      var key = theEvent.keyCode || theEvent.which;
-      key = String.fromCharCode(key);
-    } else key = '';
+    var BigCursor = {};
 
-    var currentCaption = $('#caption').html();
-    var shorterCaption = currentCaption.substring(0, currentCaption.length - 1);
-    $('#caption').html(shorterCaption + key + "|");
-    $('#fakeStartingBlinkingCursor').html('');
+    BigCursor.fakeCursorInterval = -1;
 
-    $("#toMove").animate({
-      opacity: 0,
-      margin: -100,
-      fontSize: 300,
-      left: 0
-    }, "fast");
+    document.onkeypress = function (e) {
+        if (fakeText && editor.getValue() !== "") {
+            BigCursor.shrinkFakeText(e);
+        }
+    };
 
-    setTimeout('$("#formCode").animate({opacity: 1}, "fast");', 120);
-    setTimeout('$("#justForFakeCursor").hide();', 200);
-    setTimeout('$("#toMove").hide();', 200);
-    //setTimeout('clearTimeout(fakeCursorInterval);',200);
-    fakeText = false;
+    BigCursor.shrinkFakeText = function (e) {
+        var theEvent, key, currentCaption, shorterCaption;
 
-  }
+        if (e !== undefined) {
+            theEvent = e || window.event;
+            key = theEvent.keyCode || theEvent.which;
+            key = String.fromCharCode(key);
+        } else {
+            key = '';
+        }
 
-var fakeCursorInterval;
+        currentCaption = $('#caption').html();
+        shorterCaption = currentCaption.substring(0, currentCaption.length - 1);
+        $('#caption').html(shorterCaption + key + "|");
+        $('#fakeStartingBlinkingCursor').html('');
 
-function fakeCursorBlinking() {
-  $("#fakeStartingBlinkingCursor").animate({
-    opacity: 0.2
-  }, "fast", "swing").animate({
-    opacity: 1
-  }, "fast", "swing");
-}
+        $("#toMove").animate({
+            opacity: 0,
+            margin: -100,
+            fontSize: 300,
+            left: 0
+        }, "fast");
+
+        setTimeout('$("#formCode").animate({opacity: 1}, "fast");', 120);
+        setTimeout('$("#justForFakeCursor").hide();', 200);
+        setTimeout('$("#toMove").hide();', 200);
+        //setTimeout(BigCursor.toggleBlink,200, false);
+        fakeText = false;
+
+    };
+
+    BigCursor.fakeCursorBlinking = function () {
+        $("#fakeStartingBlinkingCursor").animate({
+            opacity: 0.2
+        }, "fast", "swing").animate({
+            opacity: 1
+        }, "fast", "swing");
+    };
+
+    BigCursor.toggleBlink = function (active) {
+        if (active) {
+            BigCursor.fakeCursorInterval = setInterval(BigCursor.fakeCursorBlinking, 800);
+        } else {
+            clearTimeout(BigCursor.fakeCursorInterval);
+        }
+    };
+
+};
