bbe79051d79808224a626a0503954f780b7cd84d
Davide Della Casa <davidedc@gmail.com>
2012-11-24 01:12:55 +0000

    added a comment on how the painting of the background is managed.

--- js/background-painting.js
+++ js/background-painting.js
@@ -1,6 +1,41 @@
 /*jslint browser: true, devel: true */
 /*global $, color, logger, backgroundSceneContext, scaledBackgroundHeight, scaledBackgroundWidth */
 
+// The user can issue multiple solid fill and gradient fill commands
+// and they are all painted on top of each other according to the
+// order they have been issued in.
+// So for example you can have one gradient and then
+// a second one painted over it that uses some transparency.
+//
+// This is why solid and gradient fills are all kept in an array
+// and each time the user issues one of the two commands, an
+// element is added to the array.
+//
+// Both solid and gradient fills are stored as elements in the
+// array, all elements are the same and accommodate for a description
+// that either case (solid/gradient).
+//
+// The background/gradients are drawn on a separate 2D canvas
+// and we avoid repainting that canvas over and over if the
+// painting commands stay the same (i.e. colors of their
+// arguments and the order of the commands) across frames.
+//
+// For quickly determining whether the order/content of the commands
+// has changed across frames,
+// a string is kept that represents the whole stack of commands
+// issued in the current frame, and similarly the "previous frame"
+// string representation is also kept.
+// So it's kind of like a simplified JSON representation if you will.
+//
+// If the strings are the same across frames, then the 2D layer of
+// the background is not repainted, otherwise the array is iterated
+// and each background/gradient is painted anew.
+//
+// Note that we are not trying to be too clever here - for example
+// a solid fill effectively invalidates the contents of the previous
+// elements of the array, so we could discard those when such
+// a command is issued.
+
 var createBackgroundPainter = function () {
 
     'use strict';
