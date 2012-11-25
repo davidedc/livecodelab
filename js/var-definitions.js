6917d9ffd29cb24761b02a0a82476266ad62383d
Guy John <guy@rumblesan.com>
2012-11-25 12:31:35 +0000

    Number prototype extension in var-definitions
    Moving global code into var-definitions

--- js/var-definitions.js
+++ js/var-definitions.js
@@ -1,3 +1,21 @@
+
+
+
+/**
+ * Extend the Number prototype
+ * This needs to stay globally defined
+ * @param func
+ * @param scope [optional]
+ */
+Number.prototype.times = function (func, scope) {
+    var v = this.valueOf();
+    for (var i = 0; i < v; i++) {
+        func.call(scope || window, i);
+    }
+};
+
+
+
 // All used by Three.js
 var stats;
 var scene;
