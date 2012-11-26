4f1c2fa6260409de4d749b719c91e1f786c27e15
Guy John <guy@rumblesan.com>
2012-11-26 14:21:17 +0000

    Moving three js init code into its own object
    Keeps track of almost all the state to do with Three JS (renderer, scene, camera etc)

--- js/init-threejs.js
+++ js/init-threejs.js
@@ -1,155 +1,148 @@
+/*jslint browser: true, devel: true */
 
-//
-//
+var createThreeJs = function (Detector, THREE, THREEx) {
 
-var composer;
-var backGroundFraction = 15;
+    'use strict';
 
-var initThreeJs = function() {
+    var ThreeJs = {},
+        backgroundScene,
+        backGroundFraction = 15,
+        pointLight;
 
-  scaledBackgroundWidth = Math.floor(window.innerWidth / backGroundFraction);
-  scaledBackgroundHeight = Math.floor(window.innerHeight / backGroundFraction);
+    ThreeJs.isWebGLUsed = false;
 
-  if (!forceCanvasRenderer && Detector.webgl && !forceCanvasRenderer) {
+    ThreeJs.composer = {};
 
-    ballDefaultDetLevel = 16;
-    sceneRenderingCanvas = document.createElement('canvas');
+    ThreeJs.sceneRenderingCanvas = {};
 
-    renderer = new THREE.WebGLRenderer({
-      canvas: sceneRenderingCanvas,
-      preserveDrawingBuffer: false,
-      // to allow screenshot
-      antialias: false,
-      premultipliedAlpha: false
-    });
+    ThreeJs.forceCanvasRenderer = false;
 
-    //renderer.autoClearColor = false;
-    //renderer.autoClear = false;
-    isWebGLUsed = true;
+    ThreeJs.scaledBackgroundWidth = Math.floor(window.innerWidth / backGroundFraction);
+    ThreeJs.scaledBackgroundHeight = Math.floor(window.innerHeight / backGroundFraction);
 
-    // this is really a bad hack, but chrome goes twice
-    // as fast with this, while safari doesn't work,
-    // and firefox is the fastest no matter what
-    if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
-      //renderer.autoClear = false;
+    if (!ThreeJs.forceCanvasRenderer && Detector.webgl) {
+
+        ThreeJs.ballDefaultDetLevel = 16;
+        ThreeJs.sceneRenderingCanvas = document.createElement('canvas');
+
+        ThreeJs.renderer = new THREE.WebGLRenderer({
+            canvas: ThreeJs.sceneRenderingCanvas,
+            preserveDrawingBuffer: false, // to allow screenshot
+            antialias: false,
+            premultipliedAlpha: false
+        });
+
+        ThreeJs.isWebGLUsed = true;
+
+        ThreeJs.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById('container').appendChild(ThreeJs.sceneRenderingCanvas);
+
+    } else {
+
+
+        // we always draw the 3d scene off-screen
+        ThreeJs.ballDefaultDetLevel = 6;
+        ThreeJs.sceneRenderingCanvas = document.createElement('canvas');
+        ThreeJs.sceneRenderingCanvasContext = ThreeJs.sceneRenderingCanvas.getContext('2d');
+        ThreeJs.renderer = new THREE.CanvasRenderer({
+            canvas: ThreeJs.sceneRenderingCanvas,
+            antialias: true, // to get smoother output
+            preserveDrawingBuffer: false // to allow screenshot
+        });
+
+
+        ThreeJs.previousRenderForBlending = document.createElement('canvas');
+        ThreeJs.previousRenderForBlending.width = window.innerWidth;
+        ThreeJs.previousRenderForBlending.height = window.innerHeight;
+        ThreeJs.previousRenderForBlendingContext = ThreeJs.previousRenderForBlending.getContext('2d');
+
+        ThreeJs.finalRenderWithSceneAndBlend = document.getElementById('finalRenderWithSceneAndBlendCanvas');
+        ThreeJs.finalRenderWithSceneAndBlend.width = window.innerWidth;
+        ThreeJs.finalRenderWithSceneAndBlend.height = window.innerWidth;
+        ThreeJs.finalRenderWithSceneAndBlendContext = ThreeJs.finalRenderWithSceneAndBlend.getContext('2d');
+
+        ThreeJs.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('container').appendChild(sceneRenderingCanvas);
-    //document.getElementById('finalRenderWithSceneAndBlendCanvas').appendChild(finalRenderWithSceneAndBlend);
 
-  } else {
-
-
-    // we always draw the 3d scene off-screen
-    ballDefaultDetLevel = 6;
-    sceneRenderingCanvas = document.createElement('canvas');
-    sceneRenderingCanvasContext = sceneRenderingCanvas.getContext('2d');
-    renderer = new THREE.CanvasRenderer({
-      canvas: sceneRenderingCanvas,
-      antialias: true,
-      // to get smoother output
-      preserveDrawingBuffer: false // to allow screenshot
-    });
-
-    //renderer.autoClear = true;
-    //renderer.setClearColorHex( 0x000000, 1 );
-
-    previousRenderForBlending = document.createElement('canvas');
-    previousRenderForBlending.width = window.innerWidth;
-    previousRenderForBlending.height = window.innerHeight;
-    previousRenderForBlendingContext = previousRenderForBlending.getContext('2d');
-
-    finalRenderWithSceneAndBlend = document.getElementById('finalRenderWithSceneAndBlendCanvas');
-    //finalRenderWithSceneAndBlend.width = scaledBackgroundWidth;
-    //finalRenderWithSceneAndBlend.height = scaledBackgroundHeight;
-    finalRenderWithSceneAndBlend.width = window.innerWidth;
-    finalRenderWithSceneAndBlend.height = window.innerWidth;
-    finalRenderWithSceneAndBlendContext = finalRenderWithSceneAndBlend.getContext('2d');
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    //document.getElementById('container').appendChild(sceneRenderingCanvas);
-  }
+    backgroundScene = document.getElementById('backGroundCanvas');
+    backgroundScene.width = ThreeJs.scaledBackgroundWidth;
+    backgroundScene.height = ThreeJs.scaledBackgroundHeight;
+    ThreeJs.backgroundSceneContext = backgroundScene.getContext('2d');
 
 
-  backgroundScene = document.getElementById('backGroundCanvas');
-  backgroundScene.width = scaledBackgroundWidth;
-  backgroundScene.height = scaledBackgroundHeight;
-  backgroundSceneContext = backgroundScene.getContext('2d');
+    ThreeJs.scene = new THREE.Scene();
+    ThreeJs.scene.matrixAutoUpdate = false;
+
+
+    // put a camera in the scene
+    ThreeJs.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 10000);
+    ThreeJs.camera.position.set(0, 0, 5);
+    ThreeJs.scene.add(ThreeJs.camera);
+
+    // temporarily, add a light
+    // add subtle ambient lighting
+    //var ambientLight = new THREE.AmbientLight(0xFF0000);
+    //scene.add(ambientLight);
+    // create a point light
+    pointLight = new THREE.PointLight(0xFFFFFF);
+
+    // set its position
+    pointLight.position.x = 10;
+    pointLight.position.y = 50;
+    pointLight.position.z = 130;
+
+    // add to the scene
+    ThreeJs.scene.add(pointLight);
+
+    // transparently support window resize
+    THREEx.WindowResize.bind(ThreeJs.renderer, ThreeJs.camera);
+
+    ThreeJs.buildPostprocessingChain = function () {
+        var renderTargetParameters,
+            renderTarget,
+            effectSaveTarget,
+            fxaaPass,
+            screenPass,
+            renderModel;
+
+        renderTargetParameters = {
+            format: THREE.RGBAFormat,
+            stencilBuffer: true
+        };
+
+        renderTarget = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, renderTargetParameters);
+        effectSaveTarget = new THREE.SavePass(new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, renderTargetParameters));
+        effectSaveTarget.clear = false;
+
+        fxaaPass = new THREE.ShaderPass(THREE.ShaderExtras.fxaa);
+        fxaaPass.uniforms.resolution.value.set(1 / window.innerWidth, 1 / window.innerHeight);
+
+        ThreeJs.effectBlend = new THREE.ShaderPass(THREE.ShaderExtras.blend, "tDiffuse1");
+        screenPass = new THREE.ShaderPass(THREE.ShaderExtras.screen);
+
+        // motion blur
+        ThreeJs.effectBlend.uniforms.tDiffuse2.texture = effectSaveTarget.renderTarget;
+        ThreeJs.effectBlend.uniforms.mixRatio.value = 0;
+
+        renderModel = new THREE.RenderPass(ThreeJs.scene, ThreeJs.camera);
+
+        ThreeJs.composer = new THREE.EffectComposer(ThreeJs.renderer, renderTarget);
+
+        ThreeJs.composer.addPass(renderModel);
+        ThreeJs.composer.addPass(ThreeJs.effectBlend);
+        ThreeJs.composer.addPass(effectSaveTarget);
+        ThreeJs.composer.addPass(screenPass);
+        screenPass.renderToScreen = true;
+    };
+
+    if (ThreeJs.isWebGLUsed) {
+        ThreeJs.buildPostprocessingChain();
+    }
 
 
 
-  // add Stats.js - https://github.com/mrdoob/stats.js
-  stats = new Stats();
-  // Align bottom-left
-  stats.getDomElement().style.position = 'absolute';
-  stats.getDomElement().style.right = '0px';
-  stats.getDomElement().style.top = '0px';
-  document.body.appendChild(stats.getDomElement());
 
-  scene = new THREE.Scene();
-  scene.matrixAutoUpdate = false;
+    return ThreeJs;
 
-
-  // put a camera in the scene
-  camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 10000);
-  camera.position.set(0, 0, 5);
-  scene.add(camera);
-
-  // temporarily, add a light
-  // add subtle ambient lighting
-  //var ambientLight = new THREE.AmbientLight(0xFF0000);
-  //scene.add(ambientLight);
-  // create a point light
-  var pointLight = new THREE.PointLight(0xFFFFFF);
-
-  // set its position
-  pointLight.position.x = 10;
-  pointLight.position.y = 50;
-  pointLight.position.z = 130;
-
-  // add to the scene
-  scene.add(pointLight);
-
-  // transparently support window resize
-  THREEx.WindowResize.bind(renderer, camera);
-
-  if (isWebGLUsed) {
-    buildPostprocessingChain();
-  }
-
-}
-
-var effectBlend;
-
-var buildPostprocessingChain = function() {
-  var renderTargetParameters = {
-    format: THREE.RGBAFormat,
-    stencilBuffer: true
-  };
-
-  var renderTarget = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, renderTargetParameters);
-  var effectSaveTarget = new THREE.SavePass(new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, renderTargetParameters));
-  effectSaveTarget.clear = false;
-
-  var fxaaPass = new THREE.ShaderPass(THREE.ShaderExtras["fxaa"]);
-  fxaaPass.uniforms['resolution'].value.set(1 / window.innerWidth, 1 / window.innerHeight);
-
-  effectBlend = new THREE.ShaderPass(THREE.ShaderExtras["blend"], "tDiffuse1");
-  var screenPass = new THREE.ShaderPass(THREE.ShaderExtras["screen"]);
-
-  // motion blur
-  effectBlend.uniforms['tDiffuse2'].texture = effectSaveTarget.renderTarget;
-  effectBlend.uniforms['mixRatio'].value = 0;
-
-  var renderModel = new THREE.RenderPass(scene, camera);
-
-  composer = new THREE.EffectComposer(renderer, renderTarget);
-
-  composer.addPass(renderModel);
-  //composer.addPass( fxaaPass );
-  composer.addPass(effectBlend);
-  composer.addPass(effectSaveTarget);
-  composer.addPass(screenPass);
-  screenPass.renderToScreen = true;
-}
+};
