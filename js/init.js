60965428d8504d159875c7788a88241b91ecae27
Davide Della Casa <davidedc@gmail.com>
2012-12-06 00:44:27 +0000

    caught a Firefox-specific trap, typing "background" makes the browser to go back to previous URL in the middle of the typing. Redefined window.back and a couple of others to avoid that.

--- js/init.js
+++ js/init.js
@@ -41,6 +41,17 @@ var startEnvironment = function () {
 
 
     LiveCodeLab = createLiveCodeLab(CodeTransformer, ThreeJs, TimeKeeper, GraphicsCommands);
+    
+    // I'm going to put this here, difficult to say where it belongs. In Firefox there
+    // is a window.back() function that takes you back to the previous page. The effect
+    // is that when one types "background", in the middle of the sentence the browser
+    // changes page. Re-defining it so that it causes no harm. This can be probably
+    // fixed a bit better once we'll have a scope dedicated to livecodelab code
+    // execution.
+    // Same applies to other functions below, to avoid tripping on them in the future.
+    window.back = function() {};
+    window.forward = function() {};
+    window.close = function() {};
 
     ProgramLoader = createProgramLoader(editor, BigCursor, LiveCodeLab, ThreeJs, GraphicsCommands);
 
