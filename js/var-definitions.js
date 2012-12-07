6295082cdeadbfef9d8d4d4ea62416cf3f26fc6b
Guy John <guy@rumblesan.com>
2012-12-07 15:54:48 +0000

    Moving definitoon of back/forward/close functions
    now in var-definitions fole which seems like a better place for global
    code

--- js/var-definitions.js
+++ js/var-definitions.js
@@ -12,6 +12,18 @@ Number.prototype.times = function (func, scope) {
 };
 
 
+// I'm going to put this here, difficult to say where it belongs. In Firefox there
+// is a window.back() function that takes you back to the previous page. The effect
+// is that when one types "background", in the middle of the sentence the browser
+// changes page. Re-defining it so that it causes no harm. This can be probably
+// fixed a bit better once we'll have a scope dedicated to livecodelab code
+// execution.
+// Same applies to other functions below, to avoid tripping on them in the future.
+window.back = function() {};
+window.forward = function() {};
+window.close = function() {};
+
+
 // The CodeMirror editor
 var editor;
 
