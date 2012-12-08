862f9f02eaa1c3880287c2f7026b0202cc9f8b19
Guy John <guy@rumblesan.com>
2012-12-08 18:37:12 +0000

    Updating event router to handle context info

--- js/events.js
+++ js/events.js
@@ -6,21 +6,30 @@ var createEventRouter = function () {
     var EventRouter = {},
         events = {};
 
-    EventRouter.bind = function (name, callback) {
+    EventRouter.bind = function (name, callback, context) {
+        var listenerInfo = {
+            callback: callback,
+            context: context
+        };
+
         if (!events[name]) {
             events[name] = [];
         }
-        events[name].push(callback);
+        events[name].push(listenerInfo);
     };
 
     EventRouter.trigger = function (name) {
-        var args, callbacks, i;
+        var args, callbacks, i, listenerInfo;
+
+        // convert the arguments object into an array
+        args = Array.prototype.slice.call(arguments);
 
         if (events[name]) {
-            args = arguments.slice(1);
+            args = args.slice(1);
             callbacks = events[name];
-            for (i = 0; i < events.length; i += 1) {
-                callbacks[i](args);
+            for (i = 0; i < callbacks.length; i += 1) {
+                var listenerInfo = callbacks[i];
+                listenerInfo.callback.apply(listenerInfo.context, args);
             }
         }
     };
