95efd1d5e29584a9c1885a566bb5ea5f94e80001
Davide Della Casa <davidedc@gmail.com>
2012-12-16 14:51:28 +0000

    added comment about the difference between THREE and ThreeJsSystem

--- js/init.js
+++ js/init.js
@@ -25,11 +25,21 @@ var startEnvironment = function (canvasElementForThreeJS, canvasForBackground, f
     */
     LiveCodeLabCore.TimeKeeper = createTimeKeeper();
     // THREE is a global defined in three.min.js and used in ShaderPass, ShaderExtras, SavePass, RenderPass, MaskPass
+    // The difference between THREE and the ThreeJsSystem initialised later is that
+    // a) THREE is the raw Three.js system without for example the blend options.
+    // b) ThreeJsSystem contains some convenience fields and abstractions, for example
+    //    it keeps the renderer (whether it's canvas-based or WebGL based) in a
+    //    "renderer" field.
+    // Several fields/methids in ThreeJsSystem are just conveniency mappings into
+    // the raw THREE object.
+    // But often in LiveCodeLab there are direct reference to THREE fields/methods.
+    // So, ThreeJsSystem provides some abstraction without attempting to be a complete
+    // abstraction layer.
     LiveCodeLabCore.THREE = THREE;
-    LiveCodeLabCore.MatrixCommands = createMatrixCommands(); // needs TimeKeeper and THREE
-    LiveCodeLabCore.ColourNames = createColours(); //no
     // needs THREE
     LiveCodeLabCore.ThreeJsSystem = createThreeJsSystem(Detector, THREEx, canvasElementForThreeJS, forceCanvasRenderer); //yes
+    LiveCodeLabCore.MatrixCommands = createMatrixCommands(); // needs TimeKeeper and THREE
+    LiveCodeLabCore.ColourNames = createColours(); //no
     // requires ThreeJsSystem
     LiveCodeLabCore.BlendControls = createBlendControls(); //yes
     LiveCodeLabCore.SoundSystem = createSoundSystem(buzz, createBowser(), createSampleBank(buzz)); // $ //yes
