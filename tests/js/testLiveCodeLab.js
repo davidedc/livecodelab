d08e065b63bc6c7a6d18f7acbf5a8341081927cc
Davide Della Casa <davidedc@gmail.com>
2012-12-19 17:34:56 +0000

    first semi-working example of the test harness. Unclear why the cube is not in the middle of the canvas...

--- tests/js/testLiveCodeLab.js
+++ tests/js/testLiveCodeLab.js
@@ -8,17 +8,38 @@ describe('ImageTest', function() {
   // Test
   it('should convert be the same image', function () {
 
-    var
-      a = new Image(),
-      b = new Image();
-    a.src = 'images/1_normal_a.jpg';
+		// The div containing this canvas is supposed to be 100% width and height,
+		// so this canvas in theory should be of the right size already. But it isn't,
+		// so we are setting the width and height here again.
+		var testCanvas = document.createElement('canvas');
+
+		testCanvas.width = 200;
+		testCanvas.height = 200;
+		    
+    var eventRouter = createEventRouter();
+    var colourNames = createColours();
+
+    var liveCodeLabCoreInstance = createLiveCodeLabCore({
+    		blendedThreeJsSceneCanvas: testCanvas,
+    		canvasForBackground: null,
+    		forceCanvasRenderer: false,
+    		eventRouter: eventRouter,
+    		statsWidget: null,
+    		testMode: true
+    	});
+    liveCodeLabCoreInstance.updateCode("box");
+    liveCodeLabCoreInstance.startAnimationLoop();
+
+
+    var a = new Image();
+    var b = new Image();
     b.src = 'images/1_normal_a.jpg';
 
-    waitsFor(function () {
-      return a.complete & b.complete;
-    }, 'image not loaded.', 2000);
+
+    waits(1000);
 
     runs(function () {
+      a = liveCodeLabCoreInstance.getForeground3DSceneImage();
       expect(a).toImageDiffEqual(b);
     });
   });
