15ca5add6d86c49425cba93ce417abea5b894fef
Davide Della Casa <davidedc@gmail.com>
2012-12-19 15:54:09 +0000

    creating a dedicated livecodelab test page pointing to a dedicated livecodelab test suite. (not actually hooked up to livecodelab though).

--- tests/pythonHelpers/openWebBrowser.py
+++ tests/pythonHelpers/openWebBrowser.py
@@ -1,2 +1,2 @@
 import webbrowser
-webbrowser.open("http://localhost:8001/tests/htmlsWithTests/imagediffTest.html")
+webbrowser.open("http://localhost:8001/tests/htmlsWithTests/imagediffTestLiveCodeLab.html")
