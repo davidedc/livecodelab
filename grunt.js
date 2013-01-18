13dfdc5b947b9059b3f37649e7c679c27224bf3d
Davide Della Casa <davidedc@gmail.com>
2013-01-18 00:46:04 +0000

    hacking the build system with a script that makes it possible to generate the coffeedoc files by commenting-out the "use strict" string

--- grunt.js
+++ grunt.js
@@ -7,25 +7,21 @@ module.exports = function (grunt) {
     var exec = require('exec');
 
     /**
-     * Task for coffedoc from https://gist.github.com/3596427
+     * Task for coffeedoc from https://gist.github.com/3596427
      */
     grunt.registerMultiTask('coffeedoc', 'Generate source documents from CoffeeScript files.', function () {
         var target = this.data.target,
             options = this.data.options || {},
-            cmds = ['coffeedoc'],
+            cmds = ['buildSystem/coffeedocHelper.sh'],
             done = this.async();
 
-        Object.keys(options).forEach(function (opt) {
-            cmds.push('--' + opt + '=' + options[opt]);
-        });
-
-        cmds.push(target);
-
+        
         exec(cmds, function (err, out, code) {
             if (code === 0) {
                 grunt.log.ok(cmds.join(' '));
                 grunt.log.ok('document created at ' + target);
             } else {
+                grunt.log.ok(cmds.join(' '));
                 grunt.fail.warn('If you want to using coffeedoc task. Please global install (option with -g) coffeedoc pakage from npm.');
             }
             done();
