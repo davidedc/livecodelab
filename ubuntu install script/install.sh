e25253ef4a08f3177ab558812e6aa612ca27d00b
Davide Della Casa <davidedc@gmail.com>
2013-04-19 17:05:07 +0100

    install script for ubuntu

---
+++ ubuntu install script/install.sh
@@ -0,0 +1,14 @@
+wget https://github.com/davidedc/livecodelab/archive/master.zip
+unzip master.zip
+cd livecodelab-master
+sudo add-apt-repository ppa:chris-lea/node.js  
+sudo apt-get update  
+sudo apt-get install nodejs
+
+sudo npm install grunt
+sudo npm install -g grunt-cli
+
+sudo apt-get install default-jre
+
+sudo npm install
+sudo npm install -g crojsdoc
