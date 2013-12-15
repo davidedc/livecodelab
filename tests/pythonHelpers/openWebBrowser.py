bd3b4e8e0b7fc8d4e2a5350d605a71b44564c972
Davide Della Casa <davidedc@gmail.com>
2013-12-15 16:09:28 +0000

    removing some redundant scripts

--- tests/pythonHelpers/openWebBrowser.py
+++
@@ -1,2 +0,0 @@
-import webbrowser
-webbrowser.open("http://localhost:8001/tests/htmlsWithTests/imagediffTestLiveCodeLab.html")
--- tests/runTests.sh
+++
@@ -1,5 +0,0 @@
-cd ..
-python -m SimpleHTTPServer 8001 &
-cd tests
-cd pythonHelpers
-python openWebBrowser.py
