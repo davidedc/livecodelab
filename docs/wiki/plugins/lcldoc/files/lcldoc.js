5c98df223b43ae274186d62b940c25ffb84ff003
Guy John <guy@rumblesan.com>
2017-07-23 21:37:43 +0100

    Fleshing out arguments table

--- docs/wiki/plugins/lcldoc/files/lcldoc.js
+++ docs/wiki/plugins/lcldoc/files/lcldoc.js
@@ -16,39 +16,60 @@ function text(text) {
   return { type: 'text', text: text };
 }
 
-function attributesTable(attrs) {
-  return element('table', [
-    element('tr', [
-      element('th', [text('inlinable')]),
-      element('td', [text(attrs.inlinable.toString())])
-    ]),
-    element('tr', [
-      element('th', [text('block scoping')]),
-      element('td', [text(attrs.blockScope.toString())])
+function attributesTable(docData) {
+  return [
+    element('h2', [text('Attributes')]),
+    element('table', [
+      element('tr', [
+        element('th', [text('type')]),
+        element('td', [text(docData.type || 'unknown')])
+      ]),
+      element('tr', [
+        element('th', [text('inlinable')]),
+        element('td', [text(docData.inlinable.toString())])
+      ]),
+      element('tr', [
+        element('th', [text('block scoping')]),
+        element('td', [text(docData['block-scope'].toString())])
+      ])
     ])
-  ]);
+  ];
 }
 
-function codeExamples(example) {
-  return codeblock(example.code);
+function argumentsTable(docData) {
+  var rows = [
+    element('tr', [
+      element('th', [text('Name')]),
+      element('th', [text('Type')]),
+      element('th', [text('Optional')]),
+      element('th', [text('Defaults')])
+    ])
+  ].concat(
+    docData.arguments.map(function(arg) {
+      console.log(arg);
+      return element('tr', [
+        element('td', [text(arg.name)]),
+        element('td', [text(arg.type)]),
+        element('td', [text(arg.optional.toString())]),
+        element('td', [text(arg.optional ? arg.defaults.join(', ') : '')])
+      ]);
+    })
+  );
+
+  return [element('h2', [text('Arguments')]), element('table', rows)];
+}
+
+function codeExample(examples, name) {
+  return [element('h2', [text('Examples')]), codeblock(examples[name])];
 }
 
 function formatDoc(docData) {
-  var argList = element(
-    'ol',
-    docData.arguments.map(function(arg) {
-      return element('li', [text(arg.name)]);
-    })
-  );
-  return [
-    text(docData.description),
-    attributesTable({
-      inlinable: docData.inlinable,
-      blockScope: docData['block-scope']
-    }),
-    argList,
-    codeblock(docData.examples.simple)
-  ];
+  var tree = [];
+  return []
+    .concat([text(docData.description)])
+    .concat(attributesTable(docData))
+    .concat(argumentsTable(docData))
+    .concat(codeExample(docData.examples, 'simple'));
 }
 
 module.exports = {
