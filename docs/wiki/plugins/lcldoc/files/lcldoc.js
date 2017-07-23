c6ba33bd118b580a10994f03755fc6c3594753ae
Guy John <guy@rumblesan.com>
2017-07-23 17:39:59 +0100

    Show more data in doc display

--- docs/wiki/plugins/lcldoc/files/lcldoc.js
+++ docs/wiki/plugins/lcldoc/files/lcldoc.js
@@ -1,5 +1,37 @@
+function element(tag, children) {
+  return { type: 'element', tag: tag, children: children };
+}
+
+function codeblock(code) {
+  return {
+    type: 'codeblock',
+    attributes: {
+      code: { type: 'string', value: code },
+      language: { type: 'string', value: 'lclang' }
+    }
+  };
+}
+
+function text(text) {
+  return { type: 'text', text: text };
+}
+
+function codeExamples(example) {
+  return codeblock(example.code);
+}
+
 function formatDoc(docData) {
-  return [{ type: 'text', text: 'title ' + docData.name }];
+  var argList = element(
+    'ol',
+    docData.arguments.map(function(arg) {
+      return element('li', [text(arg.name)]);
+    })
+  );
+  return [
+    text(docData.description),
+    argList,
+    codeblock(docData.examples.simple)
+  ];
 }
 
 module.exports = {
